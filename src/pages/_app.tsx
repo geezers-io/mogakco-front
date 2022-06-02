e2fb69dfe503cc72e4a7b7c81900550e21c0fd7d
Will Kim <will@chequer.io>
2022-06-03 01:07:06 +0900

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import 'src/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from 'styles';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
