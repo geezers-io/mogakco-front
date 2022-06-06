ca7c769383e7bafb00f5d2af8d5aca9a58adf58c
Will Kim <will@chequer.io>
2022-06-06 20:15:23 +0900

    Fix build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next';
+import styled from '@emotion/styled';
 
 const Home: NextPage = () => {
-  return <div>Hello World!</div>;
+  return <TestParagraph color="red">Hello World!</TestParagraph>;
 };
+
+const TestParagraph = styled.p<{ color: string }>`
+  color: ${(p) => p.color};
+`;
 
 export default Home;
