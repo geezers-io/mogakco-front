5f3009e8440bcbd4469744bff5d4c5b3645b52b4
Will Kim <will@chequer.io>
2022-06-25 00:21:48 +0900

    Set storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,21 @@
+import { ThemeProvider } from '@emotion/react';
+import { GlobalStyles, theme } from '../src/styles';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  ),
+];
